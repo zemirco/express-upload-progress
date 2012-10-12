da40fde0e9e63940eb87ca79190e5a409e169f30
Mirco <mirco.zeiss@gmail.com>
2012-10-12 21:19:32 +0200

    init

---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser({ 
+    keepExtensions: true, 
+    uploadDir: __dirname + '/tmp',
+    limit: '2mb'
+  }));
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', function(req, res) {
+  res.render('index');
+})
+
+app.post('/', function(req, res) {
+  deleteAfterUpload(req.files.myFile.path);
+  res.end();
+})
+
+// Start the app
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+// Private functions
+
+var fs = require('fs')
+
+var deleteAfterUpload = function(path) {
+  setTimeout( function(){
+    fs.unlink(path, function(err) {
+      if (err) console.log(err);
+      console.log('file successfully deleted');
+    })
+  }, 60 * 1000)
+}
---
+++ public/javascripts/script.js
@@ -0,0 +1,31 @@
+$(function() {
+  
+  $('input[type="submit"]').on('click', function(evt) {
+    evt.preventDefault();
+    $('div.progress').show();
+  	var formData = new FormData();
+    var file = document.getElementById('myFile').files[0];
+    formData.append('myFile', file);
+    
+		var xhr = new XMLHttpRequest();
+    
+		xhr.open('post', '/', true);
+    
+		xhr.upload.onprogress = function(e) {
+			if (e.lengthComputable) {
+				var percentage = (e.loaded / e.total) * 100;
+        $('div.progress div.bar').css('width', percentage + '%');
+			}
+		};
+    
+    xhr.onload = function() {
+      $('div.progress').hide();
+      $('strong.message').text(this.statusText)
+      $('div.alert').show();
+    }
+    
+		xhr.send(formData);
+    
+  })
+  
+});
